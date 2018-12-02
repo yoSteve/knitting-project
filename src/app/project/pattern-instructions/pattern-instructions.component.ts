efffe9291de833fa5a8df92b95f9136660297c04
SteveYo <steve.yorke@gmail.com>
2018-12-01 23:33:50 -0800

    updated PatternInstructions component

--- src/app/project/pattern-instructions/pattern-instructions.component.ts
+++ src/app/project/pattern-instructions/pattern-instructions.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../project.type';
+import { ProjectService } from '../project.service';
 
 @Component({
   selector: 'knit-pattern-instructions',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pattern-instructions.component.scss']
 })
 export class PatternInstructionsComponent implements OnInit {
+  project_id;
+  project: Project;
+  mainNeedle = []; // defined
+  cuffNeedle = '**TBD**';
+  bodyCastOn = '**TBD**';
+  sleeveCastOn = '**TBD**';
+  cuffColor = '**TBD**'; // defined
+  bodyColor = '**TBD**'; // defined
+  collarColor = '**TBD**'; // defined
+  bodyPatternChart = '**TBD**'; // defined
+  sleevePatternChart = '**TBD**'; // defined
+  yokePatternChart = '**TBD**'; // defined
+  chestMeasurments = []; // defined ***
+  torsoMeasurements = []; // defined ***
+  sleeveMeasurements; // defined ***
+  guageWidth = '10'; // defined ***
+  guageHeight = '10'; // defined ***
+  guageStitches = '18'; // defined ***
+  guageRows = '24'; // defined ***
+  cuffIncrease = '**TBD**';
+  sleeveIncreases = '**TBD**';
+  sleeveIncreaseRound = '**TBD**';
+  sleeveStitchesFinal = '**TBD**';
+  underarmStitches = '**TBD**';
+  chestAndBackStitches = '**TBD**';
+  initialYokeStitches = '**TBD**';
+  finalYokeStitches = '**TBD**';
+  collarDecreases = '**TBD**';
+  collarStitches = '**TBD**';
 
-  constructor() { }
+  constructor(private projectService: ProjectService) { }
 
   ngOnInit() {
+  }
+
+  getProject(id) {
+    this.projectService.getProject(id)
+      .then(proj => this.project = proj)
+      .then(() => {
+        this.mainNeedle = this.project.guage.needles;
+        // this.chestMeasurments = this.project.measurements.chest;
+      });
   }
 
 }
--- src/app/project/project.module.ts
+++ src/app/project/project.module.ts
@@ -5,11 +5,13 @@ import { ProjectRoutingModule } from './project-routing.module';
 import { ProjectComponent } from './project/project.component';
 import { ProjectSetupComponent } from './project-setup/project-setup.component';
 import { PatternInstructionsComponent } from './pattern-instructions/pattern-instructions.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ProjectComponent, ProjectSetupComponent, PatternInstructionsComponent],
   imports: [
     CommonModule,
+    FormsModule,
     ProjectRoutingModule
   ]
 })
