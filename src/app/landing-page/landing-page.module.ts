e5cc1a1c7dced04a4b07650b09ef730c263f6721
SteveYo <steve.yorke@gmail.com>
2018-12-01 22:21:20 -0800

    routing for landing page and not-found

--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page.component';
+import { LandingPageRoutingModule } from './landing-page-routing.module';
 
 @NgModule({
   declarations: [LandingPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    LandingPageRoutingModule
   ]
 })
 export class LandingPageModule { }
--- src/app/not-found/not-found.module.ts
+++ src/app/not-found/not-found.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NotFoundComponent } from './not-found.component';
+import { NotFoundRoutingModule } from './not-found-routing.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [NotFoundComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    NotFoundRoutingModule
   ]
 })
 export class NotFoundModule { }
