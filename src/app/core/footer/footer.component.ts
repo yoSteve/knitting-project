0115cfb34b2ab41ce64ae0910c73ef398f3c29ef
SteveYo <steve.yorke@gmail.com>
2018-12-01 22:33:30 -0800

    added footer

--- src/app/core/footer/footer.component.ts
+++ src/app/core/footer/footer.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'knit-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
+  @Input() title: string;
+  @Input() author: string;
+  @Input() githubURL: string;
+  @Input() twitterURL: string;
+  @Input() instagramURL: string;
+  @Input() year: number|string;
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor() {}
 
 }
