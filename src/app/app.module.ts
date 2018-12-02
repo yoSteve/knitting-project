3603e200689cc2fa1d1879b7f5f717bcb7f57ba1
SteveYo <steve.yorke@gmail.com>
2018-12-01 22:46:09 -0800

    project routing works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { ProjectModule } from './project/project.module';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     CoreModule,
+    ProjectModule,
     AppRoutingModule // imported last to allow Module routes to be matched first
   ],
   providers: [],
--- src/app/project/project-routing.module.ts
+++ src/app/project/project-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProjectSetupComponent } from './project-setup/project-setup.component';
+import { ProjectComponent } from './project/project.component';
+import { PatternInstructionsComponent } from './pattern-instructions/pattern-instructions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'project',
+    component: ProjectComponent,
+    children: [
+      { path: 'new', component: ProjectSetupComponent },
+      // { path: ':id', component: ProjectOverviewComponent } /* Project Overview */
+      // { path: ':id/edit', component: ProjectEditComponent } /* Edit Project params */
+      // { path: ':id/design', component: ProjectDesignComponent } /* Project Design view */
+      { path: ':id/print', component: PatternInstructionsComponent } /* Print ready pattern view  */
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
