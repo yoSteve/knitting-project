e64ed4cb8fc74ce1aeda0d52080bbec1bff61843
SteveYo <steve.yorke@gmail.com>
2019-03-03 13:14:00 -0800

    testing cache in UserState

--- src/app/user/state/user.state.spec.ts
+++ src/app/user/state/user.state.spec.ts
@@ -1,21 +1,26 @@
 import { TestBed, async } from '@angular/core/testing';
 import { NgxsModule, Store } from '@ngxs/store';
 import { UserState, UserStateModel } from './user.state';
-import { SetCurrentUser } from './user.actions';
-import { identifierModuleUrl } from '@angular/compiler';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { SetCurrentUser, SetProjectOwner } from './user.actions';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UserService } from '../user.service';
 import { HttpClient } from 'selenium-webdriver/http';
+
+const user1 = {
+  id: 'user1',
+  name: 'Steve',
+  email: 'steve.yorke@gmail.com'
+};
+
+const PREV_STATE: UserStateModel = {
+  currentProjectOwner: user1,
+  currentUser: user1
+};
 
 describe('User store', () => {
   let store: Store;
   let service: UserService;
   class EmptyService {}
-  const user1 = {
-    id: 'user1',
-    name: 'Steve',
-    email: 'steve.yorke@gmail.com'
-  };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -60,4 +65,39 @@ describe('User store', () => {
         });
     });
   }); // end setProjectOwner
-});
+
+  describe('given a previous state', () => {
+    let spy;
+    const userId = user1.id;
+
+    beforeEach(async() => {
+      store.reset(PREV_STATE);
+      spy = spyOn(service, 'getUser');
+    });
+
+    describe('#setCurrentUser', () => {
+      it('should return user from cache if ids match', async() => {
+        store.dispatch(new SetCurrentUser(userId));
+        store.selectOnce((state: UserStateModel) => state.currentUser)
+          .subscribe(user => {
+            expect(spy).not.toHaveBeenCalled();
+            expect(user.id).toBe(userId);
+            expect(user.name).toBe(user1.name);
+          });
+      });
+    }); // end #setCurrentUser
+
+    describe('#setProjectOwner', () => {
+      it('should return user from cache is ids match', async () => {
+        store.dispatch(new SetProjectOwner(userId));
+        store.selectOnce((state: UserStateModel) => state.currentProjectOwner)
+          .subscribe(user => {
+            expect(spy).not.toHaveBeenCalled();
+            expect(user.id).toBe(userId);
+            expect(user.name).toBe(user1.name);
+          });
+      });
+    });
+  }); // end given previous state
+
+}); // end UserStore
